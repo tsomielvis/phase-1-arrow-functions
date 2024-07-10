1f692b101e1082f8a4a03b4ac226ebf079831302
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-07-10 20:52:31 +0300

    latest

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(a, b) {
+    return a / b ; '20'
+}
+const square = function(a) {
+    return a * a ;
+}
+const squareArrow = (a) => {
+    return a * a ;
+}
+const add = function(a, b) {
+    return a + b ;
+}
+const addArrow =(a, b) => {
+    return a + b ;
+}
